e767ecaae87593640acb5251835ff8970e8f52e0
Russell Rockwood <russell.rockwood@gmail.com>
2021-06-06 22:17:28 -0500

    Add: update/delte note functionality

--- ElevenNote2.WebAPI/Controllers/NoteController.cs
+++ ElevenNote2.WebAPI/Controllers/NoteController.cs
@@ -46,5 +46,30 @@ namespace ElevenNote2.WebAPI.Controllers
             var note = noteService.GetNoteById(id);
             return Ok(note);
         }
+
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateNoteService();
+
+            if (!service.DeleteNote(id))
+            {
+                return InternalServerError();
+            }
+
+            return Ok("Your note hath been destroyed");
+        }
     }
 }
